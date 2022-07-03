f92572380eea9841f4b09900f5236d79e02873f1
sunny <sunnyshah1899@gmail.com>
2022-07-03 20:56:05 +0530

    1

---
+++ Count number of elements in JavaScript array.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+function
+myFunction(a){ 
+    return a.length
+};
+a = "1234567890";
+console.log(a.length);
