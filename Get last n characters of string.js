cdea3dace66309e1e9aa5ec9802c00bc23289269
sunny <sunnyshah1899@gmail.com>
2022-07-03 20:56:12 +0530

    1

---
+++ Get last n characters of string.js
@@ -0,0 +1,13 @@
+//Get last n characters of string
+//- used for selecting characters from last to begun.
+
+function myFunction(str) {
+   
+    return str.slice(-3)
+ };
+ let a = "my name is sunny"
+ console.log(a.slice(0,10))// o start from my and ends to s 10.
+ console.log(a.slice(-5)) // 0 start from y.
+
+let ab = [1, 2, 3];
+console.log(ab)
